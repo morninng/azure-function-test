cbd706ca2284d2ba979ed1a1de78750850cae8d4
morninng <mr_morninng_star_dash@yahoo.co.jp>
2022-01-14 16:18:19 +0900

    timer trigger added

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -8,7 +8,13 @@ def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
+    logging.info('<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>');
+    logging.info('<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>');
+    logging.info('<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>');
+
+
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
